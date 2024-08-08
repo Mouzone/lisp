1d5fe3d6201454cd930c5cd22607dad9fdc9b7da
Mouzone <sunnyliu010@gmail.com>
2024-08-08 14:58:15 -0400

    Add preprocessing to compile different versions depending on windows, linux

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy,buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
 
@@ -14,14 +35,9 @@ int main(int argc, char** argv) {
 
         /* Output our prompt */
         char* input = readline("lispy> ");
-
-        /* Add input to history */
         add_history(input);
 
-        /* Echo input back to user */
         printf("No you're a %s\n", input);
-
-        /* Free retrieved input */
         free(input);
     }
 
